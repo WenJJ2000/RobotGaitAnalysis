8d2a3b014ac2f363f8d37d8e0bbc00d845461685
wenjj2000 <wenjj2000@gmail.com>
2025-05-12 00:15:09 +0800

    fix - get camera image in test_cam

--- arducam/test_cam.py
+++ arducam/test_cam.py
@@ -1,9 +1,16 @@
-from picamera2 import Picamera2, Preview
-import time
+from picamera2 import Picamera2
+import cv2
+
 picam2 = Picamera2()
-camera_config = picam2.create_preview_configuration()
-picam2.configure(camera_config)
-picam2.start_preview(Preview.QTGL)
+picam2.preview_configuration.main.size = (640, 480)
+picam2.preview_configuration.main.format = "RGB888"
+picam2.configure("preview")
 picam2.start()
-time.sleep(2)
-picam2.capture_file("test.jpg")
+
+while True:
+    frame = picam2.capture_array()
+    cv2.imshow("Camera", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cv2.destroyAllWindows()
